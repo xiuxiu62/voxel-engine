b4c887bfcbad6d774c5a09366ad94cddcf93f954
xiuxiu62 <jacremer@live.com>
2024-07-09 00:47:34 -0700

    adds chunk height

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,7 @@ use bevy::{
         render_asset::RenderAssetUsages,
         settings::{Backends, RenderCreation, WgpuSettings},
         texture::Image,
+        view::GpuCulling,
         RenderPlugin,
     },
     time::Time,
@@ -63,11 +64,11 @@ fn setup(
     // chunks: Query<&Chunk>,
 ) {
     (0..3)
-        .flat_map(|x| (0..3).map(move |z| Chunk::new(vec3(x as f32, 0.0, z as f32), 8)))
+        .flat_map(|x| (0..3).map(move |z| Chunk::new(vec3(x as f32, 0.0, z as f32))))
         .for_each(|mut chunk| {
-            for x in 0..8 {
-                for y in 0..8 {
-                    for z in 0..8 {
+            for x in 0..Chunk::WIDTH {
+                for y in 0..Chunk::HEIGHT {
+                    for z in 0..Chunk::WIDTH {
                         chunk.set(x, y, z, Voxel { id: 1 });
                     }
                 }
@@ -76,11 +77,14 @@ fn setup(
             commands.spawn(chunk);
         });
 
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_translation(vec3(0.0, 0.0, -10.0))
-            .looking_at(Vec3::ZERO, Vec3::Y),
-        ..Default::default()
-    });
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_translation(vec3(0.0, 0.0, -10.0))
+                .looking_at(Vec3::ZERO, Vec3::Y),
+            ..Default::default()
+        },
+        GpuCulling,
+    ));
 
     commands.spawn(DirectionalLightBundle {
         directional_light: DirectionalLight {
@@ -108,19 +112,19 @@ fn render_chunks(
     chunk_query: Query<&Chunk>,
 ) {
     chunk_query.iter().for_each(|chunk| {
-        for x in 0..chunk.size {
-            for y in 0..chunk.size {
-                for z in 0..chunk.size {
+        for x in 0..Chunk::WIDTH {
+            for y in 0..Chunk::HEIGHT {
+                for z in 0..Chunk::WIDTH {
                     if chunk.get(x, y, z).is_none() {
                         return;
                     }
 
                     let transform = Transform::from_xyz(
                         (Voxel::SIZE * x as f32)
-                            + (Voxel::SIZE * chunk.size as f32 * chunk.position.x),
+                            + (Voxel::SIZE * Chunk::WIDTH as f32 * chunk.position.x),
                         Voxel::SIZE * y as f32,
                         (Voxel::SIZE * z as f32)
-                            + (Voxel::SIZE * chunk.size as f32 * chunk.position.z),
+                            + (Voxel::SIZE * Chunk::WIDTH as f32 * chunk.position.z),
                     );
 
                     commands.spawn(PbrBundle {
@@ -155,16 +159,17 @@ impl Voxel {
 struct Chunk {
     voxels: Vec<Voxel>,
     position: Vec3,
-    size: usize,
 }
 
 impl Chunk {
+    const WIDTH: usize = 16;
+    const HEIGHT: usize = 16;
+
     #[inline]
-    pub fn new(position: Vec3, size: usize) -> Self {
+    pub fn new(position: Vec3) -> Self {
         Self {
-            voxels: vec![Voxel { id: 0 }; size * size * size],
+            voxels: vec![Voxel { id: 0 }; Self::WIDTH * Self::WIDTH * Self::HEIGHT],
             position,
-            size,
         }
     }
 
@@ -174,7 +179,7 @@ impl Chunk {
     }
 
     pub fn set(&mut self, x: usize, y: usize, z: usize, value: Voxel) {
-        if x < self.size && y < self.size && z < self.size {
+        if x < Self::WIDTH && y < Self::HEIGHT && z < Self::WIDTH {
             let i = self.flatten_cartesian(x, y, z);
             self.voxels[i] = value;
         }
@@ -182,14 +187,14 @@ impl Chunk {
 
     #[inline]
     const fn flatten_cartesian(&self, x: usize, y: usize, z: usize) -> usize {
-        (z * self.size * self.size) + (y * self.size) + x
+        (z * Self::WIDTH * Self::WIDTH) + (y * Self::HEIGHT) + x
     }
 }
 
 fn handle_input(
     timer: Res<Time>,
     keys: Res<ButtonInput<KeyCode>>,
-    mut app_exit_events: ResMut<Events<AppExit>>,
+    app_exit_events: ResMut<Events<AppExit>>,
     mut camera: Query<&mut Transform, With<Camera3d>>,
 ) {
     // let (mut velocity, transform) = query.single_mut();
@@ -234,140 +239,120 @@ fn handle_input(
 }
 
 pub fn generate_cube_mesh() -> Mesh {
-    Mesh::new(
-        PrimitiveTopology::TriangleList,
-        RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD,
-    )
-    .with_inserted_attribute(
-        Mesh::ATTRIBUTE_POSITION,
-        // Each array is an [x, y, z] coordinate in local space.
-        // The camera coordinate space is right-handed x-right, y-up, z-back. This means "forward" is -Z.
-        // Meshes always rotate around their local [0, 0, 0] when a rotation is applied to their Transform.
-        // By centering our mesh around the origin, rotating the mesh preserves its center of mass.
-        vec![
-            // top (facing towards +y)
-            [-0.5, 0.5, -0.5], // vertex with index 0
-            [0.5, 0.5, -0.5],  // vertex with index 1
-            [0.5, 0.5, 0.5],   // etc. until 23
-            [-0.5, 0.5, 0.5],
-            // bottom   (-y)
-            [-0.5, -0.5, -0.5],
-            [0.5, -0.5, -0.5],
-            [0.5, -0.5, 0.5],
-            [-0.5, -0.5, 0.5],
-            // right    (+x)
-            [0.5, -0.5, -0.5],
-            [0.5, -0.5, 0.5],
-            [0.5, 0.5, 0.5], // This vertex is at the same position as vertex with index 2, but they'll have different UV and normal
-            [0.5, 0.5, -0.5],
-            // left     (-x)
-            [-0.5, -0.5, -0.5],
-            [-0.5, -0.5, 0.5],
-            [-0.5, 0.5, 0.5],
-            [-0.5, 0.5, -0.5],
-            // back     (+z)
-            [-0.5, -0.5, 0.5],
-            [-0.5, 0.5, 0.5],
-            [0.5, 0.5, 0.5],
-            [0.5, -0.5, 0.5],
-            // forward  (-z)
-            [-0.5, -0.5, -0.5],
-            [-0.5, 0.5, -0.5],
-            [0.5, 0.5, -0.5],
-            [0.5, -0.5, -0.5],
-        ],
-    )
-    // Set-up UV coordinates to point to the upper (V < 0.5), "dirt+grass" part of the texture.
-    // Take a look at the custom image (assets/textures/array_texture.png)
-    // so the UV coords will make more sense
-    // Note: (0.0, 0.0) = Top-Left in UV mapping, (1.0, 1.0) = Bottom-Right in UV mapping
-    .with_inserted_attribute(
-        Mesh::ATTRIBUTE_UV_0,
-        vec![
-            // Assigning the UV coords for the top side.
-            [0.0, 0.2],
-            [0.0, 0.0],
-            [1.0, 0.0],
-            [1.0, 0.2],
-            // Assigning the UV coords for the bottom side.
-            [0.0, 0.45],
-            [0.0, 0.25],
-            [1.0, 0.25],
-            [1.0, 0.45],
-            // Assigning the UV coords for the right side.
-            [1.0, 0.45],
-            [0.0, 0.45],
-            [0.0, 0.2],
-            [1.0, 0.2],
-            // Assigning the UV coords for the left side.
-            [1.0, 0.45],
-            [0.0, 0.45],
-            [0.0, 0.2],
-            [1.0, 0.2],
-            // Assigning the UV coords for the back side.
-            [0.0, 0.45],
-            [0.0, 0.2],
-            [1.0, 0.2],
-            [1.0, 0.45],
-            // Assigning the UV coords for the forward side.
-            [0.0, 0.45],
-            [0.0, 0.2],
-            [1.0, 0.2],
-            [1.0, 0.45],
-        ],
-    )
-    // For meshes with flat shading, normals are orthogonal (pointing out) from the direction of
-    // the surface.
-    // Normals are required for correct lighting calculations.
-    // Each array represents a normalized vector, which length should be equal to 1.0.
-    .with_inserted_attribute(
-        Mesh::ATTRIBUTE_NORMAL,
-        vec![
-            // Normals for the top side (towards +y)
-            [0.0, 1.0, 0.0],
-            [0.0, 1.0, 0.0],
-            [0.0, 1.0, 0.0],
-            [0.0, 1.0, 0.0],
-            // Normals for the bottom side (towards -y)
-            [0.0, -1.0, 0.0],
-            [0.0, -1.0, 0.0],
-            [0.0, -1.0, 0.0],
-            [0.0, -1.0, 0.0],
-            // Normals for the right side (towards +x)
-            [1.0, 0.0, 0.0],
-            [1.0, 0.0, 0.0],
-            [1.0, 0.0, 0.0],
-            [1.0, 0.0, 0.0],
-            // Normals for the left side (towards -x)
-            [-1.0, 0.0, 0.0],
-            [-1.0, 0.0, 0.0],
-            [-1.0, 0.0, 0.0],
-            [-1.0, 0.0, 0.0],
-            // Normals for the back side (towards +z)
-            [0.0, 0.0, 1.0],
-            [0.0, 0.0, 1.0],
-            [0.0, 0.0, 1.0],
-            [0.0, 0.0, 1.0],
-            // Normals for the forward side (towards -z)
-            [0.0, 0.0, -1.0],
-            [0.0, 0.0, -1.0],
-            [0.0, 0.0, -1.0],
-            [0.0, 0.0, -1.0],
-        ],
-    )
-    // Create the triangles out of the 24 vertices we created.
-    // To construct a square, we need 2 triangles, therefore 12 triangles in total.
-    // To construct a triangle, we need the indices of its 3 defined vertices, adding them one
-    // by one, in a counter-clockwise order (relative to the position of the viewer, the order
-    // should appear counter-clockwise from the front of the triangle, in this case from outside the cube).
-    // Read more about how to correctly build a mesh manually in the Bevy documentation of a Mesh,
-    // further examples and the implementation of the built-in shapes.
-    .with_inserted_indices(Indices::U32(vec![
+    let vertices = vec![
+        // top (+y)
+        [-0.5, 0.5, -0.5],
+        [0.5, 0.5, -0.5],
+        [0.5, 0.5, 0.5],
+        [-0.5, 0.5, 0.5],
+        // bottom   (-y)
+        [-0.5, -0.5, -0.5],
+        [0.5, -0.5, -0.5],
+        [0.5, -0.5, 0.5],
+        [-0.5, -0.5, 0.5],
+        // right    (+x)
+        [0.5, -0.5, -0.5],
+        [0.5, -0.5, 0.5],
+        [0.5, 0.5, 0.5],
+        [0.5, 0.5, -0.5],
+        // left     (-x)
+        [-0.5, -0.5, -0.5],
+        [-0.5, -0.5, 0.5],
+        [-0.5, 0.5, 0.5],
+        [-0.5, 0.5, -0.5],
+        // back     (+z)
+        [-0.5, -0.5, 0.5],
+        [-0.5, 0.5, 0.5],
+        [0.5, 0.5, 0.5],
+        [0.5, -0.5, 0.5],
+        // forward  (-z)
+        [-0.5, -0.5, -0.5],
+        [-0.5, 0.5, -0.5],
+        [0.5, 0.5, -0.5],
+        [0.5, -0.5, -0.5],
+    ];
+
+    let uvs = vec![
+        // Assigning the UV coords for the top side.
+        [0.0, 0.2],
+        [0.0, 0.0],
+        [1.0, 0.0],
+        [1.0, 0.2],
+        // Assigning the UV coords for the bottom side.
+        [0.0, 0.45],
+        [0.0, 0.25],
+        [1.0, 0.25],
+        [1.0, 0.45],
+        // Assigning the UV coords for the right side.
+        [1.0, 0.45],
+        [0.0, 0.45],
+        [0.0, 0.2],
+        [1.0, 0.2],
+        // Assigning the UV coords for the left side.
+        [1.0, 0.45],
+        [0.0, 0.45],
+        [0.0, 0.2],
+        [1.0, 0.2],
+        // Assigning the UV coords for the back side.
+        [0.0, 0.45],
+        [0.0, 0.2],
+        [1.0, 0.2],
+        [1.0, 0.45],
+        // Assigning the UV coords for the forward side.
+        [0.0, 0.45],
+        [0.0, 0.2],
+        [1.0, 0.2],
+        [1.0, 0.45],
+    ];
+
+    let normals = vec![
+        // Normals for the top side (towards +y)
+        [0.0, 1.0, 0.0],
+        [0.0, 1.0, 0.0],
+        [0.0, 1.0, 0.0],
+        [0.0, 1.0, 0.0],
+        // Normals for the bottom side (towards -y)
+        [0.0, -1.0, 0.0],
+        [0.0, -1.0, 0.0],
+        [0.0, -1.0, 0.0],
+        [0.0, -1.0, 0.0],
+        // Normals for the right side (towards +x)
+        [1.0, 0.0, 0.0],
+        [1.0, 0.0, 0.0],
+        [1.0, 0.0, 0.0],
+        [1.0, 0.0, 0.0],
+        // Normals for the left side (towards -x)
+        [-1.0, 0.0, 0.0],
+        [-1.0, 0.0, 0.0],
+        [-1.0, 0.0, 0.0],
+        [-1.0, 0.0, 0.0],
+        // Normals for the back side (towards +z)
+        [0.0, 0.0, 1.0],
+        [0.0, 0.0, 1.0],
+        [0.0, 0.0, 1.0],
+        [0.0, 0.0, 1.0],
+        // Normals for the forward side (towards -z)
+        [0.0, 0.0, -1.0],
+        [0.0, 0.0, -1.0],
+        [0.0, 0.0, -1.0],
+        [0.0, 0.0, -1.0],
+    ];
+
+    let indices = Indices::U32(vec![
         0, 3, 1, 1, 3, 2, // triangles making up the top (+y) facing side.
         4, 5, 7, 5, 6, 7, // bottom (-y)
         8, 11, 9, 9, 11, 10, // right (+x)
         12, 13, 15, 13, 14, 15, // left (-x)
         16, 19, 17, 17, 19, 18, // back (+z)
         20, 21, 23, 21, 22, 23, // forward (-z)
-    ]))
+    ]);
+
+    Mesh::new(
+        PrimitiveTopology::TriangleList,
+        RenderAssetUsages::MAIN_WORLD | RenderAssetUsages::RENDER_WORLD,
+    )
+    .with_inserted_attribute(Mesh::ATTRIBUTE_POSITION, vertices)
+    .with_inserted_attribute(Mesh::ATTRIBUTE_UV_0, uvs)
+    .with_inserted_attribute(Mesh::ATTRIBUTE_NORMAL, normals)
+    .with_inserted_indices(indices)
 }
